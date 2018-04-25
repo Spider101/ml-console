64bf42432305812b53f47b67c59c6809a5f4c4bc
Abhimanyu Banerje <banerjee.abhimanyu3@gmail.com>
2018-04-25 19:26:06 -0400

    update: connect store to component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+
 import './index.css';
 // import App from './App';
 import Layout from './components/Layout';
 import registerServiceWorker from './registerServiceWorker';
+import  store  from './store'
+// import { fetch_projects } from './actions/projectActions'
 
-ReactDOM.render(<Layout />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+    	<Layout />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
