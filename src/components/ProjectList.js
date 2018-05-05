c78b3250d056b7f241acf718a82ce3a4afec2513
Abhimanyu Banerje <banerjee.abhimanyu3@gmail.com>
2018-05-05 15:44:23 -0400

    simplify code and refactor form code out into individual containers/components

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,20 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Project from './Project'
 
-const ProjectList = ({ projects, fetched, fetching ,err }) => {
+const ProjectList = ({ items }) => {
 	const project_list_template = (
 		<div>
 			<h3> Projects </h3>
 			<ul>
-				{projects.map(project =>
-					<Project key={project._id}
-						{...project} />
-				 )}
+				{
+					items.map(
+						item => <Project key={item.id}
+								{...item} />
+					)
+				}
 			</ul>
 		</div>
 	)
 	const loading_template = <h3>Loading Projects ..</h3>
-	return projects.length > 0 ? project_list_template : loading_template
+	const template = (
+		<div>
+			{items.length > 0 ? project_list_template : loading_template}
+		</div>
+	)
+	return template
 }
 
 export default ProjectList
--- src/containers/ProjectListWrapper.js
+++ src/containers/ProjectListWrapper.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import ProjectList from '../components/ProjectList'
-import { fetch_projects } from '../actions/projectActions'
+import ProjectFormWrapper from './ProjectFormWrapper'
+import * as project from '../actions/projectActions'
 
 const mapStateToProps = state => ({ ...state.projects })
 
 const mapDispatchToProps = dispatch => ({
-    fetchProjects : () => dispatch(fetch_projects())
+    fetchProjects : () => dispatch(project.fetch_projects()),
 })
 
 class ProjectListWrapper extends Component{
-    componentWillMount(){
+	componentWillMount(){
         this.props.fetchProjects()
     }
-    
-    render(){
-        return <ProjectList {...this.props}/>
+   	render(){
+		return (
+            <div>
+				<ProjectList {...this.props}/>
+				<ProjectFormWrapper/>
+			</div>
+		)
     }
 }
+
 export default connect(
     mapStateToProps,
     mapDispatchToProps
