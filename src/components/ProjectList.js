8ed2bef202be947c3cf001e89df76fd9d8bb638c
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2018-05-17 16:37:12 +0000

    update: refactor project list wrapper to be just a smart component and make project list a stateful dumb component

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,47 +1,102 @@
-import React from 'react'
+import React, { Component }from 'react'
 import PropTypes from 'prop-types'
 
+import Modal from 'material-ui/Modal'
 import { LinearProgress } from 'material-ui/Progress'
 import Grid from 'material-ui/Grid'
 import Button from 'material-ui/Button'
-import AddIcon from '@material-ui/icons/Add'
+import Icon from 'material-ui/Icon'
+import grey from 'material-ui/colors/grey'
 
 import { withStyles } from 'material-ui/styles'
 
-import Project from './Project'
+import Item from './Item'
+import ProjectFormWrapper from '../containers/ProjectFormWrapper'
 
 const styles = theme => ({
+    colorPrimary: {
+        backgroundColor: grey[300]
+    },
+    barColorPrimary: {
+        backgroundColor: grey[900]
+    },
+    item: {
+        flexGrow: 0
+    },
 	add_btn: {
 		bottom: '5%',
 		right: '5%',
 		position: 'fixed'
 	}
 })
-const ProjectList = props => {
-	const {
-		classes,
-		items, 
-		handleClick
-	} = props
 
-	const project_list_template = (
-			items.map((entry, _idx) => (
-				<Grid key={_idx} item xs>
-					<Project { ...props } project={entry}/>
-				</Grid>
-			))
-	)
-	return (
-		<div>
-			<Grid container spacing={0}>
-				{items.length > 0 ? project_list_template : <LinearProgress />}
-			</Grid>
-			<Button variant='fab' color="primary" className={classes.add_btn}
-				aria-label="add" onClick={ handleClick }>
-				<AddIcon />
-			</Button>
-		</div>
-	)
+class ProjectList extends Component{
+    state = {
+        isModalOpen: false
+    }
+	
+	componentWillMount(){
+        this.props.fetchProjects()
+    }
+	
+	handleOpen = () => {
+        const {
+            itemInEdit,
+            cancelItemEdit
+        } = this.props
+
+        this.setState({ isModalOpen: true })
+
+        if(itemInEdit.length > 0 ){ cancelItemEdit() }
+    }
+	
+	handleClose = () => this.setState({ isModalOpen: false })
+
+	prepEditForm = id =>{
+		this.props.initEditMode(id)
+		this.handleOpen()
+	}
+	
+   	render(){
+        const {
+            classes,
+            itemInEdit,
+            items,
+            deleteItem,
+            loading,
+            ...other
+        } = this.props
+		
+        const formHeading = `${itemInEdit.length > 0 ? 'Edit' : 'Add'} Project`
+        
+        const loadingBar =  <LinearProgress classes={{ 
+                                colorPrimary: classes.colorPrimary,
+                                barColorPrimary: classes.barColorPrimary
+                            }} />
+		return (
+            <div>
+                { loading && loadingBar }
+                <Grid container spacing={0}>
+                    {items.map((entry, _idx) => (
+                        <Grid key={ _idx } className={ classes.item } item xs>
+                            <Item { ...other } data={entry}
+                                handleEdit={ id => this.prepEditForm(id) }
+                                handleDelete={ id => deleteItem(id) }/>
+                        </Grid>
+                    ))}
+                </Grid>
+                <Button variant='fab' color="primary" className={classes.add_btn}
+                    aria-label="add" onClick={ () => this.handleOpen() }>
+                    <Icon> add </Icon>
+                </Button>
+				<Modal open={ this.state.isModalOpen }
+					onClose={ () => this.handleClose() }>
+					<ProjectFormWrapper 
+						headerText={ formHeading }/>
+				</Modal>	
+			</div>
+		)
+    }
 }
 
 export default withStyles(styles)(ProjectList)
--- src/containers/ProjectListWrapper.js
+++ src/containers/ProjectListWrapper.js
@@ -1,63 +1,20 @@
-import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import ProjectList from '../components/ProjectList'
-import ProjectFormWrapper from './ProjectFormWrapper'
-import * as project from '../actions/projectActions'
 
-import Modal from 'material-ui/Modal'
+import * as project from '../actions/projectActions'
+import ProjectList from '../components/ProjectList'
 
 const mapStateToProps = state => ({ ...state.projects })
 
 const mapDispatchToProps = dispatch => ({
-    fetchProjects : () => dispatch(project.fetch_projects()),
+    fetchProjects: () => dispatch(project.fetch_projects()),
+    addProject: newItem => dispatch(project.add_project(newItem)),
 	initEditMode: id => dispatch(project.init_edit_mode(id)),
+    cancelItemEdit: () => dispatch(project.cancel_project_edit()),
+    updateProject: editedItem => dispatch(project.update_project(editedItem)),
 	deleteProject: id => dispatch(project.delete_project(id))
 })
-
-class ProjectListWrapper extends Component{
-	constructor(){
-		super()
-		this.state = {
-			isModalOpen: false
-		}
-	}
-	
-	componentWillMount(){
-        this.props.fetchProjects()
-    }
-	
-	handleOpen = () => {
-		this.setState({ isModalOpen: true })
-	}
-	
-	handleClose = () => {
-		this.setState({ isModalOpen: false })
-	}
-	
-	prepEditForm = id =>{
-		this.props.initEditMode(id)
-		this.handleOpen()
-	}
-	
-   	render(){
-		const formHeading = this.props.itemInEdit.length > 0 ? "Edit" : "Add"
-		return (
-            <div>
-				<ProjectList { ...this.props }
-					handleClick={ () => this.handleOpen() }
-					handleEdit={ id => this.prepEditForm(id) }
-					handleDelete={ id => this.props.deleteProject(id) }/>
-				<Modal open={ this.state.isModalOpen }
-					onClose={ () => this.handleClose() }>
-					<ProjectFormWrapper 
-						headerText={ `${formHeading} Project` }/>
-				</Modal>	
-			</div>
-		)
-    }
-}
 
 export default connect(
     mapStateToProps,
     mapDispatchToProps
-)(ProjectListWrapper)
+)(ProjectList)
