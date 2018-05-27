3a6abe3130321d0c38fc34740225c62b61d52693
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2018-05-27 22:41:35 +0000

    add: evaluation component and wrapper

---
+++ src/components/Evaluation.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import Card, { CardHeader, CardActions, CardContent } from 'material-ui/Card'
+import Button from 'material-ui/Button'
+import Typography from 'material-ui/Typography'
+
+import { withStyles } from 'material-ui/styles'
+
+const styles = theme => ({
+	card: {
+		margin: theme.spacing.unit * 2,
+        width: 400
+	},
+    card_content: {
+        paddingTop: 0,
+        paddingBottom: 0
+    },
+    card_actions: {
+        padding: [[8, 0]]
+    }
+})
+
+const Evaluation = ({ classes, data, handleDelete }) => {
+        
+    return (
+        <Card className={classes.card}> 
+            <CardHeader title={ data.metric_name } />
+            <CardContent className={ classes.card_content }>
+                <Typography component='p'>
+                    The model trained with job id { data.job_id } achieved a performance of { data.metric_value } when evaluated on this metric
+                </Typography>
+            </CardContent>
+            <CardActions className={ classes.card_actions }>
+                <Button color='primary' size='medium'
+                    onClick={ () => handleDelete(data.id) }>
+                    Delete</Button>
+            </CardActions>
+        </Card> 
+    )
+}
+
+export default withStyles(styles)(Evaluation)
---
+++ src/containers/EvaluationListWrapper.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux'
+
+import * as evaluation from '../actions/evaluationActions'
+import ProjectList from '../components/ProjectList'
+
+const mapStateToProps = state => ({ ...state.evaluations, itemType: 'evaluation' })
+
+const mapDispatchToProps = dispatch => ({
+    fetchItems: () => dispatch(evaluation.fetch_evaluations()),
+    addItem: newItem => dispatch(evaluation.add_evaluation(newItem)),
+	initEditMode: id => dispatch(evaluation.init_edit_mode(id)),
+    cancelItemEdit: () => dispatch(evaluation.cancel_evaluation_edit()),
+    updateItem: editedItem => dispatch(evaluation.update_evaluation(editedItem)),
+	deleteItem: id => dispatch(evaluation.delete_evaluation(id))
+})
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ProjectList)
